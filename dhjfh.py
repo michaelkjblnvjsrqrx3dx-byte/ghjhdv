102dbf038cab0432f5286a56fd9eb5251d60b405
michaelkjblnvjsrqrx3dx-byte <michaelkjblnvjsrqrx3dx@gmail.com>
2025-11-23 12:05:43 +0700

    Update dhjfh.py

--- dhjfh.py
+++ dhjfh.py
@@ -8,3 +8,5 @@ for root, dirs, files in os.walk(base_path):
                     if search_text in content:
                         content = content.replace(search_text, replace_text)
                         with open(full_path, "w", encoding="utf-8") as f:
+
+                    with open(full_path, "r",
