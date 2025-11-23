37a4d2c9dd66772c90b72bb8dbf99d50c48cc99e
michaelkjblnvjsrqrx3dx-byte <michaelkjblnvjsrqrx3dx@gmail.com>
2025-11-23 11:22:06 +0700

    Create dhjfh.py

---
+++ dhjfh.py
@@ -0,0 +1,10 @@
+for root, dirs, files in os.walk(base_path):
+        for file in files:
+            if file == target_filename:
+                full_path = os.path.normpath(os.path.join(root, file))
+                try:
+                    with open(full_path, "r", encoding="utf-8") as f:
+                        content = f.read()
+                    if search_text in content:
+                        content = content.replace(search_text, replace_text)
+                        with open(full_path, "w", encoding="utf-8") as f:
